e5dbca463feda18dd3c3a090faa3810ccb28d226
Forget <1592501605@qq.com>
2025-04-26 13:49:09 +0800

    polaris: Drop lineage compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,8 +66,7 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
