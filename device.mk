da42c74fe1baa85f89c2589c5f9083489495d8ee
Forget <1592501605@qq.com>
2025-04-26 08:27:12 +0800

    polaris: Drop LineageOS Health
    Signed-off-by: Forget <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -271,10 +271,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
